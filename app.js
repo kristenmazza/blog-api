78880c2d16463897fdfd030f4d3083b12d0fefee
kristenmazza <kbmazza@gmail.com>
2023-09-28 13:44:44 -0700

    Update cors

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ async function main() {
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
